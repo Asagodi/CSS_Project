454e9957d832d4d66ea6dc315e28393aae9236f3
devrieskevin <devrieskevin@live.nl>
2018-06-24 19:12:33 +0200

    Inhibitory neurons possibility added to IF model

--- integrate_and_fire.py
+++ integrate_and_fire.py
@@ -118,8 +118,10 @@ class LHG_integrate_and_fire_model:
     Leakage and synapse recovery are both applied using the values at the start 
     of the time step for the time integration.
     
-    Inhibition is implemented by ignoring Dale's principle and taking a percentage
-    of the synapses to be inhibitory (instead of a percentage of neurons)
+    Inhibition is implemented in two possible ways: 
+    (1) Ignoring Dale's principle and taking a percentage of the 
+    synapses to be inhibitory or (2) obeying Dale's principle and
+    taking a percentage of the neurons to be inhibitory.
     
     TODO:
     - Implement leak terms - done (Levina claims: no relevant dynamics changes)
@@ -127,7 +129,8 @@ class LHG_integrate_and_fire_model:
     - Implement inhibitory neurons - done (Levina claims: no relevant dynamics changes)
     """
     
-    def __init__(self,network,v_ext=0.025,v_th=1,u=0.2,a=0.5,nu=10,tl=40,C=0.98,leakage=False,p_inh=0.0):
+    def __init__(self,network,v_ext=0.025,v_th=1,u=0.2,a=0.5,
+                 nu=10,tl=40,C=0.98,leakage=False,p_inh=0.0,inh_type="neuron"):
         """
         network: networkx network object
             Network used for the simulation
@@ -156,6 +159,9 @@ class LHG_integrate_and_fire_model:
                 
         p_inh: float
             Percentage of inhibitory synapses
+        inh_type: string ("neuron" or "synapse")
+            Type of inhibition: per synapse (ignoring Dale's principle)
+            or per neuron (obeying Dale's principle)
         """
         
         # Set parameters
@@ -175,23 +181,34 @@ class LHG_integrate_and_fire_model:
         # Link indices in weight matrix
         self.link_idx = (self.w != 0)
         
-        # Determine neuron types
-        self.neuron_types = self.w.astype(int)
-        n_inh = int(p_inh * self.neuron_types.size)
+        # Network size
+        self.N = self.w.shape[0]
         
-        type_vals = np.ones(self.neuron_types.size)
-        if n_inh > 0:
-            inhibitory = np.random.choice(np.arange(self.neuron_types.size),size=n_inh,replace=False)
-            type_vals[inhibitory] = -1
+        # Determine synapse types
+        self.synapse_types = self.w.astype(int)
+        
+        # Inhibition determined per synapse
+        if inh_type == "synapse":
+            n_inh = int(p_inh * self.synapse_types.size)
+
+            type_vals = np.ones(self.synapse_types.size)
+            if n_inh > 0:
+                inhibitory = np.random.choice(np.arange(self.synapse_types.size),size=n_inh,replace=False)
+                type_vals[inhibitory] = -1
+
+            self.synapse_types[self.link_idx] = type_vals
+        
+        # Inhibition determined per neuron
+        elif inh_type == "neuron":
+            n_inh = int(p_inh * self.N)
             
-        self.neuron_types[self.link_idx] = type_vals
+            if n_inh > 0:
+                inhibitory = np.random.choice(np.arange(self.N),size=n_inh,replace=False)
+                self.synapse_types[:,inhibitory] *= -1
         
         # Randomize synaptic connection strengths
         self.w = self.w.astype(float)
         self.w[self.link_idx] = np.random.uniform(0,self.a/self.u,self.link_idx.size)
-        
-        # Network size
-        self.N = self.w.shape[0]
         
         # Synaptic recovery time-scale
         self.tj = self.nu * self.N
@@ -236,7 +253,7 @@ class LHG_integrate_and_fire_model:
 
                         # Spiking results in firing potential to neighbors
                         # Inhibitory neurons subtract potential instead of adding
-                        self.v[j] += self.neuron_types[j,i].toarray().flatten() * \
+                        self.v[j] += self.synapse_types[j,i].toarray().flatten() * \
                                      self.u * self.w[j,i].toarray().flatten() / n
                         
                         # Decrease synaptic connection strength
